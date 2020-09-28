e116e3a28565e0cf7a9935f584f35996d61bf34c
june <junearagoncillo@gmail.com>
2020-09-28 13:09:50 +0800

    added vform on edit modal

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,27 @@
 require("./bootstrap");
 
 window.Vue = require("vue");
-import Form from "./Form";
-window.Form = Form;
+// import Form from "./Form";
+import { Form, HasError, AlertError } from 'vform'
+import Swal from 'sweetalert2'
 
+window.Form = Form;
+window.Swal = Swal;
+
+
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+})
+
+window.Toast = Toast
 Vue.component("todo-component",require("./components/TodoComponent.vue").default);
 
 const app = new Vue({
